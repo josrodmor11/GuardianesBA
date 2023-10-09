8e0f846447a87bb643bc9a6c7c4ce82edf68df62
MIT-FS <irm@us.es>
2023-10-09 16:59:10 +0200

    Elimino duplicados

--- guardianes-service/src/main/java/us/dit/service/security/ClearPasswordService.java
+++
@@ -1,9 +0,0 @@
-package us.dit.service.security;
-/**
- * Interfaz de un único método que devuelve la password a partir del nombre de usuario
- */
-public interface ClearPasswordService {
-
-	String getPwd(String key);
-
-}
